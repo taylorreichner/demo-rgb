96af111fae52c6100536304a1534ecffadfbadee
taylorreichner <taylorreichner@yahoo.com>
2021-03-22 14:16:58 -0700

    used dot notation for first test

--- name.js
+++ name.js
@@ -1,24 +1,7 @@
-
-
-
-
-
-const getName = ({name}) => {
-
-
-
-return name;
-
+const getName = (obj) => {
+    const theName = obj.name;
+    return theName;
 
 }
-
-
-
-
-
-
-
-
-
 
 module.exports = getName;
